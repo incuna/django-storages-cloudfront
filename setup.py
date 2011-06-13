7076e28da5ff66af24d4d9ced71d5ab6b011e7cf
Max Peterson <max@incuna.com>
2011-06-13 17:39:03 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = "cloudfront",
+    packages = ["cloudfront", ],
+    include_package_data=True,
+    install_requires=[
+        "django-storages",
+    ],
+    version = "0.1",
+    description = "Extend django-storages storages.backends.s3.S3Storage to use a Amazone CloudFront domain for urls.",
+    author = "Incuna Ltd",
+    author_email = "admin@incuna.com",
+    url = "http://incuna.com/",
+)
